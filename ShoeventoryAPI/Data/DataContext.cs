07cd050a78414b162161dfe8dbd1ddc1fd6ff926
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-06-28 13:05:17 -0400

    Set data context

--- ShoeventoryAPI/Data/DataContext.cs
+++ ShoeventoryAPI/Data/DataContext.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShoeventoryAPI.Models;
 
 namespace ShoeventoryAPI.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Merchant> Merchants { get; set; }
+        public DbSet<Shoe> Shoes { get; set; }
+        public DbSet<ShoeCollection> ShoeCollections { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<SoldShoe> SoldShoes { get; set; }
+
     }
 }
