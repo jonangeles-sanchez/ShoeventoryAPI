82a787b523fde30b367bd75ec9fb1f2d374aaf05
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-01 14:05:49 -0400

    Create merchant

--- ShoeventoryAPI/Models/Merchant.cs
+++ ShoeventoryAPI/Models/Merchant.cs
@@ -1,12 +1,21 @@
-﻿namespace ShoeventoryAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeventoryAPI.Models
 {
     public class Merchant
     {
         public int Id { get; set; }
+
+        [Required]
         public string MerchantName { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-        public ICollection<ShoeCollection> ShoeCollections { get; set; }
-        public ICollection<Transaction> Transactions { get; set; }
+
+        public List<ShoeCollection> ShoeCollections { get; set; }
     }
 }
