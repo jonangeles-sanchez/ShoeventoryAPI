f53a9f30a8d0cdb1ac8aa2e62946599964dc0742
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-01 14:07:24 -0400

    Create collection dto

---
+++ ShoeventoryAPI/DTOs/CollectionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeventoryAPI.DTOs
+{
+    public class CollectionDto
+    {
+        public string ShoeCollectionName { get; set; }
+        public int MerchantId { get; set; }
+    }
+}
