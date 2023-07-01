067b0ffc995796ad7141d12831a99202b7c63d5d
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-01 14:06:13 -0400

    Create ShoeCollection

--- ShoeventoryAPI/Models/ShoeCollection.cs
+++ ShoeventoryAPI/Models/ShoeCollection.cs
@@ -1,11 +1,21 @@
-﻿namespace ShoeventoryAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ShoeventoryAPI.Models
 {
     public class ShoeCollection
     {
         public int Id { get; set; }
+
+        [Required]
         public string ShoeCollectionName { get; set; }
+
         public int MerchantId { get; set; }
+
+        [JsonIgnore]
         public Merchant Merchant { get; set; }
-        public ICollection<Shoe> Shoes { get; set; }
+
+        public List<Shoe> Shoes { get; set; }
+
     }
 }
