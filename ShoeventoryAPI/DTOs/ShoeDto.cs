5e77d99c1e5829be4df10c1762c485715c68b435
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-01 14:07:31 -0400

    Create ShoeDto

---
+++ ShoeventoryAPI/DTOs/ShoeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeventoryAPI.DTOs
+{
+    public class ShoeDto
+    {
+        public int ShoeCollectionId { get; set; }
+        public string Manufacturer { get; set; }
+        public string ShoeType { get; set; }
+        public string ShoeName { get; set; }
+        public double ShoeSize { get; set; }
+        public string ShoeColor { get; set; }
+        public int ShoeQuantity { get; set; }
+        public double ShoePrice { get; set; }
+    }
+}
