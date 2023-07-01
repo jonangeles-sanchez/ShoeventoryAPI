d079a4f0fe8e8b6f2a4e8b7dcfa15c004300227d
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-01 14:07:38 -0400

    Create merchant Dto

---
+++ ShoeventoryAPI/DTOs/MerchantDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeventoryAPI.DTOs
+{
+    public class MerchantDto
+    {
+        public string MerchantName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
