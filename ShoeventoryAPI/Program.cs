4e1ce64b9b0594b529c2f6f049c12e0f79ace9e4
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-04 17:17:06 -0400

    Remove duplicate authorization add

--- ShoeventoryAPI/Program.cs
+++ ShoeventoryAPI/Program.cs
@@ -39,8 +39,6 @@ builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
     }));
 
 
-builder.Services.AddAuthorization();
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
