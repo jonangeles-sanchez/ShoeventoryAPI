b4e48db690de787662bad1c9751a37db2a754d56
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-05 13:19:01 -0400

    Create interface methods

--- ShoeventoryAPI/Services/AuthService/IAuthService.cs
+++ ShoeventoryAPI/Services/AuthService/IAuthService.cs
@@ -1,6 +1,11 @@
-﻿namespace ShoeventoryAPI.Services.AuthService
+﻿using ShoeventoryAPI.Models;
+using ShoeventoryAPI.DTOs;
+
+namespace ShoeventoryAPI.Services.AuthService
 {
     public class IAuthService
     {
+        Task<Merchant> Register(MerchantDto merchantDto);
+        Task<Boolean> UserExists(string email);
     }
 }
