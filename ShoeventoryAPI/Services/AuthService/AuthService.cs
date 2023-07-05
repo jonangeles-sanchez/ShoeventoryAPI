097c45dead94d1b7915c5fc6753e069398aac634
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-05 13:19:12 -0400

    Implement register method

--- ShoeventoryAPI/Services/AuthService/AuthService.cs
+++ ShoeventoryAPI/Services/AuthService/AuthService.cs
@@ -1,6 +1,29 @@
-﻿namespace ShoeventoryAPI.Services.AuthService
+﻿using ShoeventoryAPI.Models;
+using ShoeventoryAPI.DTOs;
+
+namespace ShoeventoryAPI.Services.AuthService
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly DataContext _db;
+
+        public AuthService(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Merchant> Register(MerchantDto merchantDto)
+        {
+            var merchant = new Merchant
+            {
+                MerchantName = merchantDto.MerchantName,
+                Password = merchantDto.Password,
+                Email = merchantDto.Email
+            };
+            _db.Merchants.Add(merchant);
+            await _db.SaveChangesAsync();
+            return merchant;
+        }
+
     }
 }
