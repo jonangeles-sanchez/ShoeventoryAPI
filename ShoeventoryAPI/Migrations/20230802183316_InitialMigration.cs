b0f02f38b353b1deb1e38b3cece72208ec2b6628
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-08-02 14:46:43 -0400

    Create migration

---
+++ ShoeventoryAPI/Migrations/20230802183316_InitialMigration.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ShoeventoryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Merchants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MerchantName = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Merchants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoeCollections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ShoeCollectionName = table.Column<string>(type: "text", nullable: false),
+                    MerchantId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoeCollections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoeCollections_Merchants_MerchantId",
+                        column: x => x.MerchantId,
+                        principalTable: "Merchants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Manufacturer = table.Column<string>(type: "text", nullable: false),
+                    ShoeType = table.Column<string>(type: "text", nullable: false),
+                    ShoeName = table.Column<string>(type: "text", nullable: false),
+                    ShoeSize = table.Column<double>(type: "double precision", nullable: false),
+                    ShoeColor = table.Column<string>(type: "text", nullable: false),
+                    ShoeQuantity = table.Column<int>(type: "integer", nullable: false),
+                    ShoePrice = table.Column<double>(type: "double precision", nullable: false),
+                    ShoeCollectionId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shoes_ShoeCollections_ShoeCollectionId",
+                        column: x => x.ShoeCollectionId,
+                        principalTable: "ShoeCollections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoeCollections_MerchantId",
+                table: "ShoeCollections",
+                column: "MerchantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shoes_ShoeCollectionId",
+                table: "Shoes",
+                column: "ShoeCollectionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shoes");
+
+            migrationBuilder.DropTable(
+                name: "ShoeCollections");
+
+            migrationBuilder.DropTable(
+                name: "Merchants");
+        }
+    }
+}
